7708fa71feb4c3b15c55363e9f126a7931c4333e
MasterGKJ <kjgao1210@gmail.com>
2017-04-18 19:22:21 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,23 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <QtCore/QCoreApplication>
+#include <QCoreApplication>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <fstream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+
+#include <get_label.h>
+#include <warp_perspective.h>
+#include <projection.h>
+#include <detect_line.h>
+#include <detect_sign.h>
+
+#endif // TEST_H
